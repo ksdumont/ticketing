9cfc405cf7f712324146f1087b178246e630f0f9
Keith Dumont <keithdumont@Keiths-MacBook-Pro.local>
2020-07-20 11:16:43 -0700

    initial commit

---
+++ auth/src/routes/current-user.ts
@@ -0,0 +1,9 @@
+import express, { Router } from 'express';
+
+const router = express.Router();
+
+router.get('/api/users/currentuser', (req, res) => {
+  res.send('hi there');
+});
+
+export { router as currentUserRouter };
---
+++ auth/src/routes/signin.ts
@@ -0,0 +1,9 @@
+import express, { Router } from 'express';
+
+const router = express.Router();
+
+router.post('/api/users/signin', (req, res) => {
+  res.send('hi there');
+});
+
+export { router as signinRouter };
